e385595d69e80e93ae27959a7f0b0e3f74a5f90b
KritikaKumawat3108 <57754090+KritikaKumawat3108@users.noreply.github.com>
2021-05-06 19:31:21 +0530

    Create class

---
+++ classes_2body.py
@@ -0,0 +1,8 @@
+class object2D:
+    def __init__(self, mass, x_initial, y_initial, vx_initial, vy_initial):
+
+        self.mass = mass
+        self.x_initial = x_initial
+        self.y_initial = y_initial
+        self.vx_initial = vx_initial
+        self.vy_initial = vy_initial
